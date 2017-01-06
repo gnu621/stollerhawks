a4d72a2cdb79df8504acece02536fffe625429b2
gnu621 <gnu621@gmail.com>
2017-01-05 21:43:22 -0800

    more changes

--- simple.c
+++ simple.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+
+int square(int a)
+{
+  return a * a;
+}
+
 int main()
 {
-   printf("Hello world");
+   printf("Hello world William@stollerhawks \n");
+
+   printf("The square of 89 is %d\n", square(89) );
+
    return 0;
 }
